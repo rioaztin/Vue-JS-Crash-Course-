aba9ae2478c413d3a1bc7053721b108794e327f7
rioaztin <100110578+rioaztin@users.noreply.github.com>
2022-03-01 16:59:31 +0700

    First Commit

---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+    <header>
+        <h1>Task tracker</h1>
+    </header>
+</template>
+
+<script>
+    export default {
+        // eslint-disable-next-line
+        name: 'Header',
+    }
+</script>
+
+<style scoped>
+    header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin-bottom: 20px;
+    }
+</style>
